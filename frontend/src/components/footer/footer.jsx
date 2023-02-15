03a1d4b7e82b7ca866c544c34f6c5729774fda52
zacharyblum2 <blum.zachary1@gmail.com>
2023-02-15 18:43:30 -0500

    Add icons to footer.

--- frontend/src/components/footer/footer.jsx
+++ frontend/src/components/footer/footer.jsx
@@ -1,31 +1,29 @@
 import React from 'react';
+import * as Icon from 'react-bootstrap-icons';
 import './footer.css';
 
 const Footer = () => {
   return (
     <footer className="text-center text-lg-start bg-light text-muted">
-      <section class="d-flex justify-content-center justify-content-lg-between p-4 border-bottom">
+      <section class="d-flex justify-content-center justify-content-lg-center align-items-center p-4 border-bottom">
         <div class="me-5 d-none d-lg-block">
           <span>Get connected with us on social networks:</span>
         </div>
         <div>
           <a href="" class="me-4 text-reset">
-            <i class="fab fa-facebook-f"></i>
+            <Icon.Facebook className='fab' style={{margin: "10px"}}/>
           </a>
           <a href="" class="me-4 text-reset">
-            <i class="fab fa-twitter"></i>
+            <Icon.Twitter style={{margin: "10px"}}/>
           </a>
           <a href="" class="me-4 text-reset">
-            <i class="fab fa-google"></i>
+            <Icon.Google style={{margin: "10px"}}/>
           </a>
           <a href="" class="me-4 text-reset">
-            <i class="fab fa-instagram"></i>
+            <Icon.Instagram style={{margin: "10px"}}/>
           </a>
           <a href="" class="me-4 text-reset">
-            <i class="fab fa-linkedin"></i>
-          </a>
-          <a href="" class="me-4 text-reset">
-            <i class="fab fa-github"></i>
+            <Icon.Linkedin style={{margin: "10px"}}/>
           </a>
         </div>
       </section>
