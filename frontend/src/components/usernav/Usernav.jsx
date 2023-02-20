6a9499f6a560df96517f311a67cf97ea23f89d45
zacharyblum2 <blum.zachary1@gmail.com>
2023-02-20 14:55:29 -0500

    Add event page custom navbar.

--- frontend/src/components/usernav/Usernav.jsx
+++ frontend/src/components/usernav/Usernav.jsx
@@ -4,16 +4,13 @@ import './usernav.css';
 const Usernav = () => {
   return (
     <nav className="navbar navbar-expand-sm navbar-dark justify-content-between">
-      <a className="navbar-brand" href="/">EventAt<span></span></a>
+      <a className="navbar-brand" href="/">EventAt: User<span></span></a>
       <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
         <span className="navbar-toggler-icon"></span>
       </button>
       <div className="collapse navbar-collapse flex-grow-0" id="navbarNavAltMarkup">
         <div className="navbar-nav">
-          <a className="nav-item nav-link active" href="/">Home <span className="sr-only">(current)</span></a>
-          <a className="nav-item nav-link" href="/universities">Universities</a>
-          <a className="nav-item nav-link" href="/signup">Sign Up</a>
-          <a className="nav-item nav-link" href="/login">Login</a>
+          <a className="nav-item nav-link active" href="/">Log out <span className="sr-only">(current)</span></a>
         </div>
       </div>
     </nav>
