54870ad26457ac77db50c1c3b4f3c39a54eccd42
zacharyblum2 <blum.zachary1@gmail.com>
2023-02-20 14:41:50 -0500

    Add event page folder structure.

--- frontend/src/pages/index.js
+++ frontend/src/pages/index.js
@@ -1,3 +1,4 @@
 export {default as LoginPage} from './loginPage/LoginPage';
 export {default as SignupPage} from './signupPage/SignupPage';
 export {default as HomePage} from './homePage/HomePage';
+export {default as User} from './user/User';
