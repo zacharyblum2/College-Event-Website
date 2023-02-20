268588c8715948e35e2c0ae22fcab78ee6dfeb6a
zacharyblum2 <blum.zachary1@gmail.com>
2023-02-20 15:05:40 -0500

    Add containers that will hold the left, right, and total view given to users.

---
+++ frontend/src/containers/rightv/Rightv.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import '/rightv.css';
+
+const Rightv = () => {
+  return (
+    <div>
+        <input type="text" placeholder='Search all events'/>
+    </div>
+  )
+}
+
+export default Rightv
