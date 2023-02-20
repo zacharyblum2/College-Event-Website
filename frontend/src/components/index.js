9bb928c7b80a6b37e8e761168f09ff2df1608231
zacharyblum2 <blum.zachary1@gmail.com>
2023-02-20 15:06:37 -0500

    Remove unused components that are muddying view.

--- frontend/src/components/index.js
+++ frontend/src/components/index.js
@@ -1,6 +1,3 @@
-export {default as Calendar} from './calendar/Calendar';
-export {default as Event} from './event/Event';
-export {default as Feed} from './feed/Feed';
 export {default as Footer} from './footer/Footer';
 export {default as NavbarComp} from './navbarcomp/NavbarComp';
 export {default as Who} from './who/Who';
