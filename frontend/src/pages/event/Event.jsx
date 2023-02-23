2cabf3e3dfceb9c33c5f3a6a6c2369bf82bc61ba
zacharyblum2 <blum.zachary1@gmail.com>
2023-02-23 12:44:01 -0500

    Create folder for event page.

---
+++ frontend/src/pages/event/Event.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import './event.css';
+
+const Event = () => {
+  return (
+    <div>Event</div>
+  )
+}
+
+export default Event
--- frontend/src/pages/index.js
+++ frontend/src/pages/index.js
@@ -2,3 +2,4 @@ export {default as LoginPage} from './loginPage/LoginPage';
 export {default as SignupPage} from './signupPage/SignupPage';
 export {default as HomePage} from './homePage/HomePage';
 export {default as User} from './user/User';
+export {default as Event} from './event/Event';
