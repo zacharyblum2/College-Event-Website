9a57bdbbae4141e45a0a83ee1747556503aecdb0
zacharyblum2 <blum.zachary1@gmail.com>
2023-03-07 13:54:28 -0500

    Add typing to inputs, required statements and min length for password.

--- frontend/src/components/signup/Signup.jsx
+++ frontend/src/components/signup/Signup.jsx
@@ -19,7 +19,7 @@ const Signup = () => {
                       <div class="d-flex flex-row align-items-center mb-4">
                         <i class="fas fa-user fa-lg me-3 fa-fw"></i>
                         <div class="form-outline flex-fill mb-0">
-                          <input type="text" id="form3Example1c" class="form-control" />
+                          <input type="text" id="form3Example1c" class="form-control" required/>
                           <label class="form-label" for="form3Example1c">Your Name</label>
                         </div>
                       </div>
@@ -27,7 +27,7 @@ const Signup = () => {
                       <div class="d-flex flex-row align-items-center mb-4">
                         <i class="fas fa-envelope fa-lg me-3 fa-fw"></i>
                         <div class="form-outline flex-fill mb-0">
-                          <input type="email" id="form3Example3c" class="form-control" />
+                          <input type="email" id="form3Example3c" class="form-control" required/>
                           <label class="form-label" for="form3Example3c">Your Email</label>
                         </div>
                       </div>
@@ -35,7 +35,7 @@ const Signup = () => {
                       <div class="d-flex flex-row align-items-center mb-4">
                         <i class="fas fa-lock fa-lg me-3 fa-fw"></i>
                         <div class="form-outline flex-fill mb-0">
-                          <input type="password" id="form3Example4c" class="form-control" />
+                          <input type="password" minlength="5" id="form3Example4c" class="form-control" required/>
                           <label class="form-label" for="form3Example4c">Password</label>
                         </div>
                       </div>
@@ -43,7 +43,7 @@ const Signup = () => {
                       <div class="d-flex flex-row align-items-center mb-4">
                         <i class="fas fa-key fa-lg me-3 fa-fw"></i>
                         <div class="form-outline flex-fill mb-0">
-                          <input type="password" id="form3Example4cd" class="form-control" />
+                          <input type="password" id="form3Example4cd" class="form-control" required/>
                           <label class="form-label" for="form3Example4cd">Repeat your password</label>
                         </div>
                       </div>
