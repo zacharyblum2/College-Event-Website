14e11efaec5a957b27330f7c853c402b47490289
zacharyblum2 <blum.zachary1@gmail.com>
2023-03-26 12:11:20 -0400

    Added password for my machine.

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -83,7 +83,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'cew',
                 'USER': 'root',
-                'PASSWORD': 'password',
+                'PASSWORD': 'pword231*1',
                 'HOST': '127.0.0.1',
                 'PORT': '3306'
     }
