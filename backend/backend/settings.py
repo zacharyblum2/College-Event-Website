588b8657fb6bfbdfebeed4a519e87aaae1d00b75
zacharyblum2 <blum.zachary1@gmail.com>
2023-03-26 12:10:04 -0400

    Change password to correct one.

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -83,7 +83,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'cew',
                 'USER': 'root',
-                'PASSWORD': 'pword231*1',
+                'PASSWORD': 'password',
                 'HOST': '127.0.0.1',
                 'PORT': '3306'
     }
