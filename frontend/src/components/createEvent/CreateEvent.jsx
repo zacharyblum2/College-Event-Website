abb8a6397692e1250c96f2274ec1677a6bd390b0
zacharyblum2 <blum.zachary1@gmail.com>
2023-04-03 16:26:09 -0400

    Setup adding event.

--- frontend/src/components/createEvent/CreateEvent.jsx
+++ frontend/src/components/createEvent/CreateEvent.jsx
@@ -4,13 +4,45 @@ import * as Icon from 'react-bootstrap-icons';
 import { Map } from '../../components';
 
 const CreateEvent = () => {
+    let eventName;
+    let eventDesc;
+    let eventRSO = -1;
+    let eventTime;
+    let eventEmail;
+    let eventType;
+    let eventPhone;
+    let eventLon;
+    let eventLat;
+    let eventLoc;
+
+    // Call getAdmin, if the array is empty keep, otherwise change to false.
+    const [admin, setAdmin] = useState(false);
 
     let user_data = JSON.parse(localStorage.getItem("user_data"));
-    // let de
+    console.log(user_data.id);
+
+    let admin_rso;
+
+    const getAdmin = async event => {
+        event.preventDefault();
+        
+        let obj = {id: user_data.id};
+        let js = JSON.stringify(obj);
+
+        // const response = await
+        //     fetch('http://127.0.0.1:8000/api/rsos/',
+        //     {method:'POST', body:js, headers: {'Content-Type': 'application/json'}});
+
+        // Depending on response, setAdmin(true)
+    }
 
     const makeEvent = async event => {
-        
-        let obj = {rso_id: 1, name: "", admin_id: user_data.id};
+        event.preventDefault();
+
+        // Grab just date from eventDate.value;
+        let obj = {name: eventName.value, description: eventDesc.value, creator: user_data.id, host_rso: eventRSO.value,
+                   date: eventTime.value.substr(0, 9), time: eventTime.value, email: eventEmail.value, event_type: eventType.value,
+                   phone: eventPhone.value, longitude: 0, latitude: 0, loc_name: 0};
         
         let js = JSON.stringify(obj);
         alert(js);
@@ -40,67 +72,79 @@ const CreateEvent = () => {
     return (
         <>
             {/* Store RSO name, and admin id (who creates it) */}
-            <form id='build' action="">
-
-                <div class="form-outline mb-4" id="back">
-                    <a href="/user" class="btn btn-outline-success"><Icon.ArrowLeft/></a>
-                </div>
+            <form id='build' action="" onLoad={getAdmin}>
 
                 <div id="containForm">
+                    <div class="form-outline mb-4" id="back">
+                        <a href="/user" class="btn btn-outline-success"><Icon.ArrowLeft/></a>
+                    </div>
+                    
                     <div id="left" class="form-outline mb-4">
                         <div class="form-outline mb-4">
                             <label class="form-label" for="rsoName">Event Name</label>
                             <input type="text" id="eventName" class="form-control" placeholder="Something Club"
-                            required/>
+                            required ref={ (c) => eventName = c}/>
                         </div>
 
                         <div class="form-outline mb-4">
                             <label class="form-label" for="email1">Event Type: </label>
-                            <select class="form-control">
+                            <select class="form-control" ref={ (c) => eventType = c }>
                                 <option value="0">Public</option>
                                 <option value="1">Private</option>
-                                <option value="2">RSO</option>
+                                { admin ? <><option value="2">RSO</option></> : <></>}
                             </select>
                         </div>
 
                         <div class="form-outline mb-4">
                             <label class="form-label" for="description">Description</label>
                             <input type="text" id="description" class="form-control" placeholder="Describe your event"
-                            required/>
+                            required ref={ (c) => eventDesc = c}/>
                         </div>
                     </div>
 
                     <div id="right" class="form-outline mb-4">
                         <div class="form-outline mb-4">
                             <label class="form-label" for="datetime">Date and Time</label>
-                            <input type="datetime-local" id="datetime" name="datetime" class="form-control"/>
-                        </div>
-
-                        <div class="form-outline mb-4">
-                            <label class="form-label" for="address">Address</label>
-                            <input type="text" id="address" class="form-control" placeholder="123 Street"
-                            required />
+                            <input type="datetime-local" id="datetime" name="datetime" class="form-control"
+                            ref={ (c) => eventTime = c}/>
                         </div>
 
                         <div class="form-outline mb-4">
                             <label class="form-label" for="phone">Contact Phone Number</label>
                             <input type="tel" id="phone" class="form-control" placeholder="123-456-789"
                             pattern="[0-9]{3}-[0-9]{3]-[0-9]{3}"
-                            required />
+                            required ref={ (c) => eventPhone = c}/>
                         </div>
 
                         <div class="form-outline mb-4">
                             <label class="form-label" for="email">Contact Email</label>
                             <input type="email" id="email" class="form-control" placeholder="123@gmail.com"
-                            required />
+                            required ref={ (c) => eventEmail = c}/>
                         </div>
+
+                        {/* Only display if the user is admin of RSO */}
+                        { admin ? 
+                            <>
+                                <div class="form-outline mb-4">
+                                    <label class="form-label" for="email">RSO</label>
+                                    <select class="form-control" ref={ (c) => eventRSO = c}>
+                                        <option value="0">RSO1</option>
+                                        <option value="1">RSO2</option>
+                                        <option value="2">RSO3</option>
+                                    </select>
+                                </div>  
+                            </>
+                        : <></>}
+
                     </div>
+                    
                     <div className="map">
+                        {/* Get eventLoc, eventLon, eventLat from Map */}
                         <h5>Pick your event location</h5>
                         <Map create={true}/>
                     </div>
-                </div>                
-
+                    
+                </div>    
                 {/* Onclick will submit data to database and return to "/rso" */}
                 <div id="makeEvent" class="form-outline mb-4 but">
                     <button type="submit" class="btn btn-success" onClick={makeEvent}>Submit</button>
