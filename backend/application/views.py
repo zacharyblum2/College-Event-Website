66c205fa15c88357fcb2cfc2f72378eed27d331b
dyrozang <gab.01@hotmail.com>
2023-04-14 11:51:57 -0400

    get_user_admin_rsos added and condition for rsos need to be active

--- backend/application/views.py
+++ backend/application/views.py
@@ -349,7 +349,7 @@ def get_user_admin_rsos(request):
         try:
             user = Users.objects.get(user_id=req_id)
             for rso in RSOS.objects.all():
-                if (rso.admin == user.user_id):
+                if ((rso.admin == user.user_id) and rso.active == 1):
                     ret["data"]["rsos"].append(rso.name)
         except ObjectDoesNotExist:
             return HttpResponseBadRequest('User, RSO, or University not found.'.\
