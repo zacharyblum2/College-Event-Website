29f0bfab4550f1856bf56c431b7f3b503f3724ee
zacharyblum2 <blum.zachary1@gmail.com>
2023-04-17 10:25:37 -0400

    Adjust create event to reflect main changes.

--- frontend/src/components/createEvent/CreateEvent.jsx
+++ frontend/src/components/createEvent/CreateEvent.jsx
@@ -57,15 +57,16 @@ const CreateEvent = () => {
 
         try {
             // Grab just date from eventDate.value;
-            let obj = {name: eventName.value, description: eventDesc.value, creator: user_data.id, host_rso: eventRso.value,
-                date: eventTime.value.substr(0, 10), time: eventTime.value, email: eventEmail.value, event_type: eventType.value,
-                phone: eventPhone.value, longitude: loc_data.lng, latitude: loc_data.lat, loc_name: loc_data.address};
+            let obj = { name: eventName.value, description: eventDesc.value, time: eventTime.value, 
+                        creator: user_data.id, host_rso: eventRso.value, date: eventTime.value.substr(0, 10), 
+                        email: eventEmail.value, event_type: eventType.value, phone: eventPhone.value, 
+                        loc_name: loc_data.address, longitude: loc_data.lng, latitude: loc_data.lat };
 
             let js = JSON.stringify(obj);
             // alert(js);
 
             const response = await
-            fetch('http://127.0.0.1:8000/api/events/',
+            fetch('http://127.0.0.1:8000/api/create_event/',
             {method:'POST', body:js, headers: {'Content-Type': 'application/json'}});
 
             let r = await response.text();
