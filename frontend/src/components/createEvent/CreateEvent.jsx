e0ef2f1ba156a71462e7b7e4f7282401ec76229c
zacharyblum2 <blum.zachary1@gmail.com>
2023-04-17 16:41:30 -0400

    Update createevnet.

--- frontend/src/components/createEvent/CreateEvent.jsx
+++ frontend/src/components/createEvent/CreateEvent.jsx
@@ -63,7 +63,7 @@ const CreateEvent = () => {
                         loc_name: loc_data.address, longitude: loc_data.lng, latitude: loc_data.lat };
 
             let js = JSON.stringify(obj);
-            // alert(js);
+            console.log(js);
 
             const response = await
             fetch('http://127.0.0.1:8000/api/create_event/',
@@ -151,7 +151,7 @@ const CreateEvent = () => {
 
                         <div class="form-outline mb-4">
                             <label class="form-label" for="phone">Contact Phone Number</label>
-                            <input type="tel" id="phone" class="form-control" placeholder="123-456-789"
+                            <input type="tel" id="phone" class="form-control" placeholder="123456789"
                             required ref={ (c) => eventPhone = c}/>
                         </div>
 
